6050704765ed38dddfe11906cd8b571093fe6e4f
Клюев <sorumond@gmail.com>
2019-06-15 14:32:20 +0300

    rework slider. Add animation

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
+let transition = 0;
+let maxTransition = 0;
+let minTransition = 0;
 
 function createSlider(id, slideCount) {
     let container = document.querySelector(`#${id}`);
-    console.log(container);
+    // console.log(container);
     container.style.position = 'relative';
-    let slideList = `<div class="slide-list">${container.innerHTML}</div>`;
+    let slideList = `<div class="slide-list">${container.innerHTML}
+${container.innerHTML}
+${container.innerHTML}</div>`;
     container.innerHTML = slideList;
-    [...container.querySelector('.slide-list').children].forEach((slide) => {
+
+    let slides = [...container.querySelector('.slide-list').children];
+    slides.forEach((slide) => {
         slide.style.minWidth = `${100 / slideCount}%`;
     });
+    transition = 0 - (slides[0].offsetWidth * (slides.length / 3));
+    let wrapper = document.querySelector('.slide-list');
+    console.log (transition);
+    wrapper.style.transform = `translateX(${transition}px)`;
     container.innerHTML += `<button class="right"></button>
         <button class="left"></button>`;
     addListener(id);
+    minTransition = transition;
+    maxTransition = transition + transition;
 }
 
 createSlider('cont', 3);
 
+
+
+let currentIndex = 1;
+console.log(minTransition);
+console.log(maxTransition);
+console.log(transition);
 function moveRight(id) {
     let container = document.querySelector(`#${id}`);
     let slides = [...container.querySelector('.slide-list').children];
+    let slideList = container.querySelector('.slide-list');
 
-    container.querySelector('.slide-list').appendChild(slides[0]);
+    transition -= slides[0].offsetWidth;
+    slideList.style.transform = `translateX(${transition}px)`;
+    console.log(transition);
+//     if (currentIndex =) {
+//         container.querySelector('.slide-list').appendChild(slides[0]);
+// }
 }
-
+console.log(transition);
 
 function moveLeft(id) {
     let container = document.querySelector(`#${id}`);
     let slides = [...container.querySelector('.slide-list').children];
-
-    container.querySelector('.slide-list').insertBefore(slides[slides.length-1], container.querySelector('.slide-list').firstChild);
+    let slideList = container.querySelector('.slide-list');
+    transition += slides[0].offsetWidth;
+    slideList.style.transform = `translateX(${transition}px)`;
+    if (transition > minTransition) {
+        console.log('Гуси Летят');
+        transition += minTransition;
+        slideList.style.transition = '';
+        slideList.style.transform = `translateX(${transition}px)`;
+        slideList.style.transition = `1.5s`;
+        console.log(transition);
+    }
 }
 
 
-function addListener (id) {
-    document.querySelector(`#${id}`).addEventListener('click', (event) => {
-       const isLeftButton = event.target.matches('.left');
-       const isRightButton = event.target.matches('.right');
 
-       if (isLeftButton) {
-           moveLeft(id);
-       } else if (isRightButton) {
-           moveRight(id);
-       }
+
+function addListener(id) {
+    document.querySelector(`#${id}`).addEventListener('click', (event) => {
+        const isLeftButton = event.target.matches('.left');
+        const isRightButton = event.target.matches('.right');
+
+        if (isLeftButton) {
+            moveLeft(id);
+        } else if (isRightButton) {
+            moveRight(id);
+        }
 
     });
 }
